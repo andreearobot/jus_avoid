015d81cdb861a9bf25c056a41872bd21f97b2750
andreearobot <andreea@therobotacademy.com>
2017-08-07 15:28:49 +0200

    Corrected

--- avoid_obstacles.py
+++ avoid_obstacles.py
@@ -4,6 +4,9 @@
 import rospy
 from std_msgs.msg import Int32
 
+import stop
+import signal
+
 # Maestro channel assignment
 izq = 4
 dcho = 5
@@ -33,31 +36,27 @@ fwFactor= 100
 turning = 1
 stopped= 0.5	
 
-class avoid():
-	def _init_(self):
+class Avoider():
+	def __init__(self):
 		# Node initialization
-		rospy.init_node('contro	l', anonymous=False)
-	 	rospy.on_shutdown(self.shutdown)
-
+		rospy.init_node('control')
+		rospy.on_shutdown(self.shutdown)
 		############################################
 		# BEGIN SUBSCRIBER: Sharp sensor
 		read_sharp = rospy.Subscriber('sharp_data', Int32, scan_callback)
 		# END SUBSCRIBER
 		############################################
 		# BEGIN PUB: left and right servos speed set
-		write_left = rospy.Publisher('speed_left', Int32,queue_size=10)
-		write_right = rospy.Publisher('speed_right', Int32,queue_size=10)
+		self.write_left = rospy.Publisher('speed_left', Int32,queue_size=10)
+		self.write_right = rospy.Publisher('speed_right', Int32,queue_size=10)
 		# END PUB
 		############################################
-
 		driving_forward = True
 		state_change = False # Detects when some motor needs to change its direction of rotation
 		#Maximum time for JUS to turn to other direction
 		state_change_time = rospy.Time.now() + rospy.Duration(fwFactor*cycle)
-
 		# BEGIN LOOP
 		rate = rospy.Rate(1/cycle)
-
 
 		while not rospy.is_shutdown():
 			# CHECK IF IT IS NEEDED TO CHANGE THE STATE BETWEEN AHEAD/TURNING
@@ -78,8 +77,8 @@ class avoid():
 				# Stop the motors and wait some time before commanding them to move again
 				speed_L= 0 #zero # 0
 				speed_R= 0 #zero # 0
-				write_left.publish(speed_L)
-				write_right.publish(speed_R)
+				self.write_left.publish(speed_L)
+				self.write_right.publish(speed_R)
 			rospy.sleep(stopped)
 			state_change = False
 
@@ -92,24 +91,26 @@ class avoid():
 				speed_L= + (spin-desv_spin) # -1
 				speed_R= + (spin-desv_spin) #-1
 
-			write_left.publish(speed_L + zero)
-			write_right.publish(speed_R + zero)
+			self.write_left.publish(speed_L + zero)
+			self.write_right.publish(speed_R + zero)
 			print "Distance = ", g_sharp_ahead
 			print "LEFT motor speed=   ", speed_L
 			print "RIGHT motor speed= ", speed_R
 			rate. sleep()
 
 	def shutdown(self):
-	        rospy.loginfo("Stop")
-		write_left.publish(0)
-		write_right.publish(0)
-	        rospy.sleep(1)
- 
-if _name_ == '_main_':
-    try:
-        avoid()
-    except:
-        rospy.loginfo("node terminated.")
+        	rospy.loginfo("Stop")
+       		self.write_right.publish(0)
+		self.write_left.publish(0)
+        	rospy.sleep(1)
+
+
+if __name__ == '__main__':
+	try:
+        	Avoider()
+	except:
+		rospy.loginfo("node terminated.")
+
 
 # END LOOP
 # END ALL
